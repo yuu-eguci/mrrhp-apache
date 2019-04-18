2d2b97142f46812ac5fe77efb84a221b29ebd905
midori-mate <midori.mate@gmail.com>
2019-04-18 17:54:09 +0900

    #12 handler500 ■ Add view for 500 error
    It works only with production.SHOW_500_ERROR True

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from app.lib import consts
+from django.shortcuts import render, redirect
+from app.usrlib import consts
+import sys
+from django.http import HttpResponse
 
 def top(request, lang):
     data = {}
@@ -29,3 +31,14 @@ def tag(request, lang, code):
 def year(request, lang, code):
     data = {}
     return render(request, 'app/list.tpl', data)
+
+
+def page_server_error(request, *args, **kw):
+
+    # Write error info in /var/log/httpd/error_log
+    # import traceback
+    # print(traceback.format_exc())
+
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponse(error_html)
