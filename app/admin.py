b31661a3443b80a41e683726eeb36de71d06b05b
midori-mate <midori.mate@gmail.com>
2019-04-20 12:08:24 +0900

    #20 model_config ■ Add model, Config

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+
+class ConfigAdmin(admin.ModelAdmin):
+
+    list_display = ('key', 'value')
+    # readonly_fields = ()
+    fields = ('key', 'value')
+
+    def key(self, instance):
+        return instance
+
+    def value(self, instance):
+        return instance
+
+admin.site.register(Config, ConfigAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Config(models.Model):
+
+    key = models.CharField(
+        verbose_name='Config key name',
+        max_length=30,
+        blank=True,
+        null=True,
+        unique=True,
+    )
+    value = models.CharField(
+        verbose_name='Config value',
+        max_length=100,
+        blank=True,
+        null=True,
+    )
