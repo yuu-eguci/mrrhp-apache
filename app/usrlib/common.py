8b3635c48ca70a278393ae8bce52ceb843e5d549
midori-mate <midori.mate@gmail.com>
2019-04-29 17:42:58 +0900

    #34 api_transplant ■ Add various util methods

--- app/usrlib/common.py
+++ app/usrlib/common.py
@@ -3,6 +3,9 @@
 """
 
 from app.models import *
+import os
+from app.usrlib import consts
+import markdown
 
 
 def _get_config_value(key):
@@ -11,3 +14,16 @@ def _get_config_value(key):
     if not row:
         return False
     return row.value
+
+
+def read_file(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding=consts.Encoding.UTF8) as f:
+            return f.read()
+    return ''
+
+
+def get_markdown_metadata(filepath):
+    md = markdown.Markdown(extensions=['meta'])
+    md.convert(read_file(filepath))
+    return md.Meta
--- app/usrlib/date_utils.py
+++ app/usrlib/date_utils.py
@@ -3,7 +3,12 @@
 """
 
 import datetime
+import os
 
 
 def get_current_year() -> str:
     return datetime.datetime.now().strftime('%Y')
+
+
+def get_current_microsecond() -> int:
+    return datetime.datetime.now().microsecond
