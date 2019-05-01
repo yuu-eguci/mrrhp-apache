3129583496f545a9e30e9a3f3e563019ee41be73
midori-mate <midori.mate@gmail.com>
2019-05-01 09:46:17 +0900

    #53 ■ Add process to display post by code

---
+++ app/bizlogic/post_bizlogic.py
@@ -0,0 +1,41 @@
+
+"""Post bizlogic
+"""
+
+from django.shortcuts import get_object_or_404
+from app.models import *
+from app.usrlib import consts, common
+
+
+def __get_post_by_code(code):
+    """Get one post by code. It occurs 404 when no post is found."""
+    return get_object_or_404(
+        Post,
+        code=code,
+        publish_at__lte=datetime.datetime.today(),
+    )
+
+
+def get_post(code, lang, require_body=False):
+    """Get one post by code. It occurs 404 when no post is found.
+    This method organizes post data for display on tpl file.
+    """
+
+    post = __get_post_by_code(code)
+    return {
+        'title'     : common.dp_lang(lang, post.title_ja, post.title_en),     # Depends on lang
+        'code'      : post.code,                                              # As it is
+        'publish_at': date_utils.format_by_lang_Ymd(lang, post.publish_at),   # Change format depends on lang
+        'thumbnail' : post.thumbnail,                                         # As it is
+        'body'      : common.dp_lang(lang,                                    # Change markdown text to html
+                                     post.get_markdownified_body_ja(),
+                                     post.get_markdownified_body_en())
+                      if require_body else '',
+        'tag'       : {                                                       # As it is.
+            'name': common.dp_lang(lang, post.tag.name_ja, post.tag.name_en),
+            'code': post.tag.code,
+        },
+        'no_en_version': not post.body_en,                                    # If has English body
+        # TODO: Make archive datetime aware and this line available.
+        # 'is_before2018': date_utils.is_before_2018(post.publish_at),
+    }
