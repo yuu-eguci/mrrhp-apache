88be36bfb8f641531b4f547e81c786c84a3c4fe2
yuu-eguci <yuu.eguci@gmail.com>
2022-05-08 21:22:08 +0900

    🚨 flake8 対応: config/settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,5 @@
 import os
-from .base import *
+from .base import *  # noqa: F403
 
 DEBUG = True
 
@@ -8,8 +8,8 @@ ALLOWED_HOSTS = ['localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: F405
     }
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # noqa: F405
